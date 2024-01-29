bbd8dede2a42138cfe2527bcdf134bf30c720c17
Abdelrahman Khaled <61363091+AbdelrahmanKK@users.noreply.github.com>
2024-01-29 04:10:21 +0200

    trial 35

--- nightwatch_tests/contactUs.js
+++ nightwatch_tests/contactUs.js
@@ -1,8 +1,6 @@
 module.exports = {
     'test insvalid input': function (browser) {
 
-
-    //   console.log(require('path').resolve(__dirname + '/file.txt'))
       console.log(__dirname + '/file.txt')
       const message_selector = "#message";
       const email_selector = "#email";
@@ -15,7 +13,7 @@ module.exports = {
         .setValue(message_selector,"hello, i hope this message finds you shiny")
         .setValue(email_selector,"hello@gmail.com")
         .setValue(subject_heading_selector,"c")
-        .setValue(attach_file_selector, __dirname + '/file.txt')
+        .setValue(attach_file_selector, __dirname + '/upload_test.txt')
         .click('#submitMessage')
         .assert.not.elementPresent('div.alert.alert-danger')
 
